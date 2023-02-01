c72f65e347c8801fa23d80e05d0d50b9cd8566b8
RokasSweets <sweetsound741@gmail.com>
2023-02-01 12:31:25 +0200

    002

---
+++ 002/app.js
@@ -0,0 +1,15 @@
+console.log('hello');
+
+window.addEventListener("load", (event) => {
+    const r = document.querySelector('h1');
+    console.log(r);
+  });
+  
+  // && ||
+
+  const A = 5;
+  const B = 7;
+
+
+  console.log('A && B', A && B);
+  console.log('A || B', A || B);
