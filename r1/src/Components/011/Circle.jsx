6f031816d99122540ac04f0311b72a1ea8b334ff
RokasSweets <sweetsound741@gmail.com>
2023-02-20 14:05:15 +0200

    hocs

--- r1/src/Components/011/Circle.jsx
+++ r1/src/Components/011/Circle.jsx
@@ -1,18 +1,18 @@
 import { useState } from "react";
 
-
 function Circle({children}) {
 
-const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-// const addCount = c => ({...c, props: {...c.props, count: count}});
+    // const addCount = c  =>  ({...c, props: {...c.props, count: count}});
+
 
     return (
         <>
-        <button className="blue" onClick={() => setCount(c => c + 1)}>PLUS 1</button>
+        <button className="blue" onClick={() => setCount(c => c + 1)}>plus 1</button>
         <div className="circle">
             {
-                children.map((n, i) => ({...n, props: {...n.props, count: count + i}}))
+                children.map((n, i) => ({...n, props: {...n.props, count: count + i }})) 
             }
         </div>
         </>
--- r1/src/Components/011/Number.jsx
+++ r1/src/Components/011/Number.jsx
@@ -1,11 +1,10 @@
 function Number({type, count}) {
 
-return (
-    <div className={'circle-number ' + type}>
-        {count}
-    </div>
-)
-
+    return (
+        <div className={'circle-number ' + type}>
+            {count}
+        </div>
+    )
 }
 
 export default Number;
