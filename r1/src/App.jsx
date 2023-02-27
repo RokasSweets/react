ead6c8a3807c1edd63e2244e2107ae0adb64d3d0
RokasSweets <sweetsound741@gmail.com>
2023-02-27 12:04:17 +0200

    sensei code

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -1,19 +1,44 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.scss';
-import Products from './Components/013/Products';
-// import Counter from './Components/013/Counter';
+import { useEffect, useState } from 'react';
+import Create from './Components/Dices/Create';
+import List from './Components/Dices/List';
+import { create, read } from './Components/Dices/localStorage';
+import './Components/Dices/style.scss';
+
+const KEY = 'FancyDices';
 
 function App() {
 
+    const [lastUpdate, setLastUpdate] = useState(Date.now());
+    const [list, setList] = useState(null);
+    const [createData, setCreateData] = useState(null);
 
+    useEffect(() => {
+
+        setTimeout(() => setList(read(KEY)), 1000);
+
+        // setList(read(KEY));
+
+    }, [lastUpdate]);
+
+    
+    useEffect(() => {
+        if (null === createData) {
+            return;
+        }
+        create(KEY, createData);
+        setLastUpdate(Date.now());
+    }, [createData]);
 
     return (
-        <div className="App">
-            <header className="App-header">
-
-            <Products />
-
-            </header>
+        <div className="dices">
+            <div className="content">
+                <div className="left">
+                    <Create setCreateData={setCreateData}/>
+                </div>
+                <div className="right">
+                    <List list={list} />
+                </div>
+            </div>
         </div>
     );
 
